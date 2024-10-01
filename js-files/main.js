9b68315de30b1d4d0b09fdb9ee753383c42c3ec4
raselworshop <raselworshop@gmail.com>
2024-10-01 14:33:09 -0400

    added the calculations

--- js-files/main.js
+++ js-files/main.js
@@ -5,6 +5,8 @@ const availableSeatEl = document.getElementById('available-seat');
 const totalPriceEl = document.getElementById('total-price');
 const couponInput = document.getElementById('coupon-field');
 const couponInputBtn = document.getElementById('coupon-btn');
+const defaultTextEl = document.getElementById('default-text');
+const grandTotalEl = document.getElementById('grand-total')
 
 let selectedSeat = [];
 let totalPrice = 0;
@@ -26,6 +28,16 @@ function handleSelectSeat(event){
     const availableSeatValues = parseFloat(availableSeatEl.innerText);
     const newAvailableSeatValues = availableSeatValues - 1;
     availableSeatEl.innerText = newAvailableSeatValues;
+
+    // default text remove 
+        defaultTextEl.classList.add('hidden')
+
+
+    // default text changes to You are booking 
+    // const newText = document.createElement('h2')
+    // newText.innerText = `You're booking seats:`;
+    // const defaultTextElCh = document.getElementById('default-text');
+    // defaultTextElCh.appendChild(newText)
 
     // booked seat after seat button clicked
     selectedSeatEl.innerHTML += `
@@ -50,3 +62,23 @@ function handleSelectSeat(event){
     }
 
 }
+
+// coupon button function
+document.getElementById('coupon-btn').addEventListener('click', function(){
+    const couponInputValue = couponInput.value;
+    let couponSave = 0;
+    console.log(couponInputValue)
+    if(couponInputValue !== 'NEW15' && couponInputValue !== 'Couple 20'){
+        alert("Your given cuopon code isn't valid!!!");
+        return;
+    }
+    
+    if(couponInputValue === 'NEW15'){
+        couponSave = totalPrice * 0.15 ;
+    }else if(couponInputValue === 'Couple 20'){
+        couponSave = totalPrice * 0.20 ;
+    }
+    // garnd total value 
+    const grandTotalValue = totalPrice - couponSave;
+    grandTotalEl.innerText = grandTotalValue.toFixed(2)
+})
